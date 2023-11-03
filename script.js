93a770ba13b375a4aa721715b549fae81f7d3363
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-03 17:58:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const sizeInfo = document.getElementById('size');
+
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    sizeInfo.textContent = `Width: ${width}, Height: ${height}`;
+}
+
+window.addEventListener('resize', updateSize);
+updateSize(); // Initial call to display initial window size
+
